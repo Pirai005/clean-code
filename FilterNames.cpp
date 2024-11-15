dfcdfdad57378a002dc75b6ce60a96f0019fa8c7
Pirai005 <44574115+Pirai005@users.noreply.github.com>
2024-11-15 10:09:11 +0530

    Update FilterNames.cpp

--- FilterNames.cpp
+++ FilterNames.cpp
@@ -2,17 +2,25 @@
 #include <vector>
 #include <string>
 
-void printNamesStartingWithB(std::vector<std::string>& names)
+std::vector<std::string> getNamesStartingWith(const std::vector<std::string>& names, char startingChar)
 {
+    std::vector<std::string> result;
     for (const auto& name : names) {
-        if (name.front() == 'B') {
-            std::cout << name << std::endl;
+        if (name.front() == startingChar) {
+            result.push_back(name);
         }
     }
+    return result;
 }
 
 int main() {
-    std::vector<std::string> names = {"Bosch", "Robert","Lenovo", "Bengaluru"};
-    printNamesStartingWithB(names);
+    std::vector<std::string> names = {"Bosch", "Robert", "Lenovo", "Bengaluru"};
+    char startingChar = 'B';
+    std::vector<std::string> namesStartingWith = getNamesStartingWith(names, startingChar);
+
+    for (const auto& name : namesStartingWith) {
+        std::cout << name << std::endl;
+    }
+
     return 0;
 }
